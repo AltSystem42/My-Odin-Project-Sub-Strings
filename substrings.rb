13ee3c49e6cc0a6560d610d0dce307496685aeb2
AltSystem42 <50642697+AltSystem42@users.noreply.github.com>
2026-01-30 10:17:06 -0400

    Made logic for substrings
    Cleans up word/sentence and splits it
    Make a new hash and returns it
    Check to see if the word is in the dictionary

--- substrings.rb
+++ substrings.rb
@@ -1,7 +1,24 @@
-def substrings
-  
+def substrings(word, dict)
+  # Clean up words into an array
+  word = word.gsub(/[^A-Za-z0-9\s]/, '').downcase.split(" ")
+  #make a hash for word counting
+  result = Hash.new(0)
+  #take each word and check if it's in the dictionary
+  word.each do |drow|
+    dict.each do |entry|
+      if drow.include?(entry)
+        result[entry] += 1
+      end
+    end
+  end
+  #return the result sorted and put back to a hash.
+  result.sort.to_h
 end
+
+
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("below", dictionary)
+
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
